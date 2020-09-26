a620e394297e5cf754eef9f3a761bf134bfc0c96
ayako0 <58671176+ayako0@users.noreply.github.com>
2020-09-26 14:44:54 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -55,5 +55,5 @@ export default function (Vue, { router, head, isClient, appOptions }) {
 }
 
 <style>
-  <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;1,300;1,400;1,500;1,600&display=swap" rel="stylesheet">
+  @import url('https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;1,300;1,400;1,500;1,600&display=swap');
 </style>
