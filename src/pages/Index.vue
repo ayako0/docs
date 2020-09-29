535d30a2632116c1a09ad20fb32f8b2d55c1dcf2
ayako0 <58671176+ayako0@users.noreply.github.com>
2020-09-28 17:01:26 -0700

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -58,7 +58,7 @@ export default {
           key: "description",
           name: "description",
           content:
-            "A theme for static site documentation based on Gridsome, ready to deploy to Netlify in one click.",
+            "Visualizing economic theory.",
         },
       ],
     };
