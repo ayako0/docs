0e6ceb9352f75fea852e449268d531143162a890
ayako0 <58671176+ayako0@users.noreply.github.com>
2020-10-01 09:43:49 -0700

    Update Doc.vue

--- src/templates/Doc.vue
+++ src/templates/Doc.vue
@@ -3,7 +3,20 @@
     <h1>
       {{ $page.doc.title }}
     </h1>
-     <div class="markdown" v-html="$page.doc.content" />
+    <div class="markdown" v-html="$page.doc.content" />
+    <footer>
+      <div class="footer-inner" id="footer-left">
+        <ul>
+          <li id="footer-year">2020</li>
+        </ul>
+      </div>
+      <div class="footer-inner" id="footer-right">
+        <ul>
+          <li id="facebook">Twitter</li>
+          <li id="instagram">Email</li>
+        </ul>
+      </div>
+    </footer>
   </Layout>
 </template>
 
@@ -24,11 +37,15 @@ export default {
     return {
       title: this.$page.doc.title,
       meta: [
-        { key: 'description', name: 'description', content: this.$page.doc.description }
-      ]
-    }
-  }
-}
+        {
+          key: "description",
+          name: "description",
+          content: this.$page.doc.description,
+        },
+      ],
+    };
+  },
+};
 </script>
 
 
@@ -49,10 +66,80 @@ export default {
 }
 
 /deep/ > p > img {
-    max-width: 100%;
-  }
+  max-width: 100%;
+}
 
 .markdown {
   padding-bottom: 40px;
+}
+
+footer {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  //box-sizing: border-box;
+  border-top: 1px solid $textBright;
+}
+
+.footer-inner {
+  background: transparent;
+  width: 50%;
+  display: flex;
+  align-items: center;
+  padding: 0;
+  color: $textBright;
+}
+
+#footer-left {
+  display: flex;
+  //justify-content: flex-end;
+}
+
+#footer-left ul {
+  display: flex;
+  list-style-type: none;
+  //justify-content: flex-end;
+  padding-inline-start: 0px;
+}
+
+#footer-left ul li {
+  padding: 0px 20px;
+  color: $textBright;
+}
+
+#footer-right {
+  display: flex;
+  justify-content: flex-end;
+}
+
+#footer-right ul {
+  display: flex;
+  list-style-type: none;
+  justify-content: flex-end;
+  padding-inline-start: 0px;
+}
+
+#footer-right ul li {
+  padding: 0px 20px;
+  color: $textBright;
+}
+
+@include respond-between(xxs, xs) {
+  footer {
+    flex-direction: column;
+    align-items: center;
+  }
+  .footer-inner {
+    justify-content: center;
+    min-width: 240px;
+  }
+  #footer-right {
+    justify-content: center;
+  }
+  #footerright ul {
+    justify-content: center;
+  }
 }
 </style>
