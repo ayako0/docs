60dcc3b98c7e152b4bc640d88d175edfa7e70bb6
ayako0 <58671176+ayako0@users.noreply.github.com>
2022-03-20 14:28:51 -0700

    Update Logo.vue

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -20,17 +20,17 @@ export default {
   props: {
     color: {
       type: String,
-      required: true
-    }
-  }
-}
+      required: true,
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 .logo {
   height: 40px;
   display: block;
-  transition: color .15s ease-in-out;
+  transition: color 0.15s ease-in-out;
   color: $textBright;
   position: relative;
   perspective: 200px;
@@ -51,15 +51,15 @@ export default {
 .square {
   width: 40px;
   height: 40px;
-  background: linear-gradient(260deg, #1aff24, #20e95c);
+  //background: linear-gradient(260deg, #1aff24, #20e95c);
+  background: linear-gradient(260deg, $gradientOne, $gradientTwo);
 }
 
-.theme-enter-active, .theme-leave-active {
-  transition: opacity .15s ease-in-out;
+.theme-enter-active,
+.theme-leave-active {
+  transition: opacity 0.15s ease-in-out;
 }
 .theme-enter, .theme-leave-to /* .fade-leave-active below version 2.1.8 */ {
   opacity: 0;
 }
 </style>
-
-
