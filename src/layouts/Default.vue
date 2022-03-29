c03d0483c3d7df8e7ccaaafb2241323425b8ae3e
ayako0 <58671176+ayako0@users.noreply.github.com>
2022-03-29 15:07:25 -0700

    Update Default.vue

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -105,7 +105,8 @@ export default {
     transform: translate(0);
     width: 100%;
     max-width: 100%;
-    //height: 100vh;
+    height: 100vh;
+    overflow: scroll;
     padding: 20px;
     background: #f1f1f1;
   }
